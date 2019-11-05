5f71863ad8cd59e1a04c72790fb3ef0df3d85163
grady <mgradysaunders@gmail.com>
2019-11-05 13:28:20 -0500

    rerun doxygen

--- docs/doxygen/html/menudata.js
+++ docs/doxygen/html/menudata.js
@@ -1,7 +1,2 @@
 var menudata={children:[
-{text:"Main Page",url:"index.html"},
-{text:"Namespaces",url:"namespaces.html",children:[
-{text:"Namespace List",url:"namespaces.html"},
-{text:"Namespace Members",url:"namespacemembers.html",children:[
-{text:"All",url:"namespacemembers.html"},
-{text:"Typedefs",url:"namespacemembers_type.html"}]}]}]}
+{text:"Main Page",url:"index.html"}]}
