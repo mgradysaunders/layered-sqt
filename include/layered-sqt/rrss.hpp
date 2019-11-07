238dafa65cadfb1ff05f421c8ab94cad80a4ad6a
grady <mgradysaunders@gmail.com>
2019-11-07 14:00:54 -0500

    more doc

--- include/layered-sqt/rrss.hpp
+++ include/layered-sqt/rrss.hpp
@@ -44,6 +44,62 @@ namespace ls {
 
 /**
  * @brief Redundancy-reduced sample set.
+ *
+ * This encapsulates an algorithm for building a _redundancy-reduced 
+ * sample set_ which, as the name suggests, is a set of sample directions
+ * fit to a target solid-angle distribution with minimal redundancy&mdash;that
+ * is, minimal unwarranted (and undesirable) clumping of sample directions.
+ *
+ * @par Algorithm
+ *
+ * Let @f$ \Omega = \{\omega_k\} @f$, where @f$ k = 1,2,\ldots,N @f$, be a
+ * set of @f$ N @f$ random sample directions. We would like to select 
+ * @f$ N' < N @f$ of these sample directions to form a subset 
+ * @f$ \Omega' \subset \Omega @f$ which fits a solid-angle density @f$ p @f$ 
+ * with minimal &ldquo;redundancy&rdquo;. In other words, we intend to discard
+ * directions from @f$ \Omega @f$ such that the inherent density of the 
+ * remaining directions better represents @f$ p @f$, a target density.
+ *
+ * We thus require a quantifiable measure of &ldquo;redundancy&rdquo; 
+ * so that we can identify the most appropriate sample directions to discard.
+ * We define such a measure by inspecting a region @f$ \mathcal{R}_k @f$
+ * around each @f$ \omega_k @f$. For each @f$ \omega_k @f$, let 
+ * @f$ m_k @f$ be the number of directions we actually find in 
+ * @f$ \mathcal{R}_k @f$,
+ * and let @f$ \bar{m}_k @f$ be the number of directions we 
+ * would expect to find in @f$ \mathcal{R}_k @f$ if @f$ \Omega @f$ 
+ * were ideally representative of @f$ p @f$,
+ * @f[
+ *      m_k = \sum_{k=1}^N \begin{cases}
+ *          1 & \omega_k \in    \mathcal{R}_k
+ *      \\  0 & \omega_k \notin \mathcal{R}_k
+ *      \end{cases}
+ * @f]
+ * @f[
+ *      \bar{m}_k 
+ *      = N \operatorname{Pr}(\mathcal{R}_k)
+ *      = N \int_{\mathcal{R}_k} p(\omega)\;\mathrm{d}\omega.
+ * @f]
+ * We define the redundancy @f$ \rho_k @f$ for each 
+ * @f$ \omega_k @f$ as 
+ * @f[
+ *      \rho_k = \frac{m_k}{\bar{m}_k}.
+ * @f]
+ * When @f$ \rho_k \approx 1 @f$, we see about as many directions 
+ * around @f$ \omega_k @f$ as we expect, and so @f$ \omega_k @f$ is 
+ * properly representative. When @f$ \rho_k \gg 1 @f$, we see many more 
+ * directions around @f$ \omega_k @f$ than we expect, and so @f$ \omega_k @f$ 
+ * is redundant. Of course, we must still consider how to form the
+ * @f$ \mathcal{R}_k @f$ and how to estimate the @f$ \bar{m}_k @f$. Before
+ * continuing however, it is important to discuss how we might apply this 
+ * measure to discard directions. To discard only the most redundant direction,
+ * we can simply select and discard the direction with the greatest
+ * @f$ \rho_k @f$. However, to discard @f$ \ell > 1 @f$ directions, we 
+ * cannot simply select and discard the top-@f$ \ell @f$ directions with
+ * greatest @f$ \rho_k @f$, as the act of discarding a direction implicitly
+ * changes the @f$ \rho_k @f$ of the remaining directions. We must instead
+ * discard the most redundant direction @f$ \ell @f$ times, updating the
+ * @f$ \rho_k @f$ in between.
  */
 class Rrss
 {
@@ -62,7 +118,7 @@ public:
         Vec3<Float> dir;
         
         /**
-         * @brief Probability density.
+         * @brief Target probability density.
          */
         Float pdf = 0;
 
@@ -90,11 +146,9 @@ public:
          * Direction.
          *
          * @param[in] pdf
-         * Probability density.
+         * Target probability density.
          */
-        Sample(const Vec3<Float>& dir, Float pdf) : 
-                dir(dir), 
-                pdf(pdf)
+        Sample(const Vec3<Float>& dir, Float pdf) : dir(dir), pdf(pdf)
         {
         }
     };
